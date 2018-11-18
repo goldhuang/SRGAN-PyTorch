15fcab596cfb2421d5d175c1140a2959ed67db73
Jin Huang <ilovehawthorn@gmail.com>
2018-11-17 21:15:23 -0800

    Fix several issues

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ from preprocess import TrainDataset, DevDataset, display_transform
 from model import Generator, Discriminator
 
 def main():
-	n_epoch_pretrain = 50
+	n_epoch_pretrain = 100
 	use_tensorboard = True
 
 	parser = argparse.ArgumentParser(description='SRGAN Train')
@@ -32,12 +32,14 @@ def main():
 	parser.add_argument('--num_epochs', default=100, type=int, help='training epoch')
 	parser.add_argument('--batch_size', default=64, type=int, help='training batch size')
 	parser.add_argument('--train_set', default='data/train', type=str, help='train set path')
+	parser.add_argument('--check_point', type=str, default='', help="check point name to load")
 
 	opt = parser.parse_args()
 
 	input_size = opt.crop_size
 	n_epoch = opt.num_epochs
 	batch_size = opt.batch_size
+	check_point = opt.check_point
 
 	train_set = TrainDataset(opt.train_set, crop_size=input_size, upscale_factor=4)
 	train_loader = DataLoader(dataset=train_set, num_workers=2, batch_size=batch_size, shuffle=True)
@@ -61,6 +63,13 @@ def main():
 	netD = Discriminator()
 	print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
 
+	if check_point != '':
+		netG.load_state_dict(torch.load('epochs/netG_' + check_point + '.pth'))
+		
+		d_path = 'epochs/netD_' + check_point + '.pth'
+		if os.path.isfile(d_path): 
+			netD.load_state_dict(torch.load())
+
 	if torch.cuda.is_available():
 		netG.cuda()
 		netD.cuda()
@@ -77,7 +86,7 @@ def main():
 	start_time = time.process_time()
 	
 	# Pre-train generator using only MSE loss
-	if True:
+	if check_point == '':
 		for epoch in range(1, n_epoch_pretrain + 1):
 			train_bar = tqdm(train_loader)
 			
@@ -112,10 +121,17 @@ def main():
 			
 			if use_tensorboard:
 				log_value('pretrain-gloss', cache['g_loss']/len(train_loader), epoch)
-			
+	
+	
+	# Save model parameters	
+	if torch.cuda.is_available():
+		torch.save(netG.state_dict(), 'epochs/netG_epoch_pre_gpu.pth')
+	else:
+		torch.save(netG.state_dict(), 'epochs/netG_epoch_pre_cpu.pth')
+		
 	pretrain_done_time = time.process_time()	
 	pretrain_time = pretrain_done_time - start_time
-	
+		
 	for epoch in range(1, n_epoch + 1):
 		train_bar = tqdm(train_loader)
 		
@@ -157,7 +173,7 @@ def main():
 			image_loss = mse(fake_img_hr, real_img_hr)
 			perception_loss = mse(netV(fake_img_hr), netV(real_img_hr))
 			adversarial_loss = bce(logits_fake, torch.ones_like(logits_fake))
-			g_loss = image_loss + 2e-6*perception_loss + 1e-3*adversarial_loss
+			g_loss = image_loss + 1e-4*perception_loss + 1e-3*adversarial_loss
 
 			cache['image_loss'] += image_loss.item()
 			cache['perception_loss'] += perception_loss.item()
@@ -181,6 +197,14 @@ def main():
 			log_value('g_loss', cache['g_loss']/len(train_loader), epoch)
 		
 		if epoch == n_epoch or epoch%5 == 0:
+			# Save model parameters	
+			if torch.cuda.is_available():
+				torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_gpu.pth' % (epoch))
+				torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_gpu.pth' % (epoch))
+			else:
+				torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_cpu.pth' % (epoch))
+				torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_cpu.pth' % (epoch))
+				
 			# Visualize results
 			if True:
 				netG.eval()
@@ -202,7 +226,7 @@ def main():
 						hr = hr.cuda()
 					sr = netG(lr)
 
-					batch_mse = ((sr - hr) ** 2).data.mean()
+					batch_mse = ((sr - hr) ** 2).data.mean().item()
 					valing_results['mse'] += batch_mse * batch_size
 					batch_ssim = pytorch_ssim.ssim(sr, hr).item()
 					valing_results['ssims'] += batch_ssim * batch_size
@@ -214,10 +238,11 @@ def main():
 					
 					cache['ssim'] += valing_results['ssim']
 					cache['psnr'] += valing_results['psnr']
-
-					dev_images.extend(
-						[display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
-						 display_transform()(sr.data.cpu().squeeze(0))])
+					
+					# Only save 5 images to avoid out of memory 
+					if len(dev_images) < 5 :
+						dev_images.extend([display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)), display_transform()(sr.data.cpu().squeeze(0))])
+				
 				dev_images = torch.stack(dev_images)
 				dev_images = torch.chunk(dev_images, dev_images.size(0) // 15)
 				
@@ -232,14 +257,6 @@ def main():
 			if use_tensorboard:			
 				log_value('ssim', cache['ssim']/len(dev_loader), epoch)
 				log_value('psnr', cache['psnr']/len(dev_loader), epoch)			
-					
-			# Save model parameters	
-			if torch.cuda.is_available():
-				torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_gpu.pth' % (epoch))
-				torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_gpu.pth' % (epoch))
-			else:
-				torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_cpu.pth' % (epoch))
-				torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_cpu.pth' % (epoch))
 	
 	train_done_time = time.process_time()	
 	train_time = train_done_time - pretrain_done_time
