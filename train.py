ebcd3de2fbb5b2145e329ca31358531c26b6ef1a
Jin Huang <ilovehawthorn@gmail.com>
2018-12-03 00:01:04 -0800

    Tweaks

--- train.py
+++ train.py
@@ -198,69 +198,67 @@ def main():
 			log_value('adv_loss', cache['adv_loss']/len(train_loader), epoch)
 			log_value('g_loss', cache['g_loss']/len(train_loader), epoch)
 		
-		if True:
-			# Save model parameters	
-			if torch.cuda.is_available():
-				torch.save(netG.state_dict(), 'cp/netG_epoch_%d_gpu.pth' % (epoch))
-				torch.save(netD.state_dict(), 'cp/netD_epoch_%d_gpu.pth' % (epoch))
-				torch.save(optimizerG.state_dict(), 'cp/optimizerG_epoch_%d_gpu.pth' % (epoch))
-				torch.save(optimizerD.state_dict(), 'cp/optimizerD_epoch_%d_gpu.pth' % (epoch))
-			else:
-				torch.save(netG.state_dict(), 'cp/netG_epoch_%d_cpu.pth' % (epoch))
-				torch.save(netD.state_dict(), 'cp/netD_epoch_%d_cpu.pth' % (epoch))
-				torch.save(optimizerG.state_dict(), 'cp/optimizerG_epoch_%d_cpu.pth' % (epoch))
-				torch.save(optimizerD.state_dict(), 'cp/optimizerD_epoch_%d_cpu.pth' % (epoch))
-				
-			# Visualize results
-			if True:
-				with torch.no_grad():
-					netG.eval()
-					out_path = 'vis/'
-					if not os.path.exists(out_path):
-						os.makedirs(out_path)
-						
-					dev_bar = tqdm(dev_loader)
-					valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
-					dev_images = []
-					for val_lr, val_hr_restore, val_hr in dev_bar:
-						batch_size = val_lr.size(0)
-						valing_results['batch_sizes'] += batch_size
-						print (val_lr.ndimension())
-						lr = Variable(val_lr)
-						hr = Variable(val_hr)
-						if torch.cuda.is_available():
-							lr = lr.cuda()
-							hr = hr.cuda()
-						
-						sr = netG(lr)
-						
-						batch_mse = ((sr - hr) ** 2).data.mean().item()
-						valing_results['mse'] += batch_mse * batch_size
-						batch_ssim = pytorch_ssim.ssim(sr, hr).item()
-						valing_results['ssims'] += batch_ssim * batch_size
-						valing_results['psnr'] = 10 * log10(1 / (valing_results['mse'] / valing_results['batch_sizes']))
-						valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
-						dev_bar.set_description(
-							desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
-								valing_results['psnr'], valing_results['ssim']))
-						
-						cache['ssim'] += valing_results['ssim']
-						cache['psnr'] += valing_results['psnr']
-						
-						# Only save 1 images to avoid out of memory 
-						if len(dev_images) < 120 :
-							dev_images.extend([to_image()(val_hr_restore.squeeze(0)), to_image()(hr.data.cpu().squeeze(0)), to_image()(sr.data.cpu().squeeze(0))])
-					
-					dev_images = torch.stack(dev_images)
-					dev_images = torch.chunk(dev_images, dev_images.size(0) // 6)
-					
-					dev_save_bar = tqdm(dev_images, desc='[saving training results]')
-					index = 1
-					for image in dev_save_bar:
-						image = utils.make_grid(image, nrow=3, padding=5)
-						utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
-						index += 1
+		# Save model parameters	
+		if torch.cuda.is_available():
+			torch.save(netG.state_dict(), 'cp/netG_epoch_%d_gpu.pth' % (epoch))
+			torch.save(netD.state_dict(), 'cp/netD_epoch_%d_gpu.pth' % (epoch))
+			torch.save(optimizerG.state_dict(), 'cp/optimizerG_epoch_%d_gpu.pth' % (epoch))
+			torch.save(optimizerD.state_dict(), 'cp/optimizerD_epoch_%d_gpu.pth' % (epoch))
+		else:
+			torch.save(netG.state_dict(), 'cp/netG_epoch_%d_cpu.pth' % (epoch))
+			torch.save(netD.state_dict(), 'cp/netD_epoch_%d_cpu.pth' % (epoch))
+			torch.save(optimizerG.state_dict(), 'cp/optimizerG_epoch_%d_cpu.pth' % (epoch))
+			torch.save(optimizerD.state_dict(), 'cp/optimizerD_epoch_%d_cpu.pth' % (epoch))
 			
+		# Visualize results
+		with torch.no_grad():
+			netG.eval()
+			out_path = 'vis/'
+			if not os.path.exists(out_path):
+				os.makedirs(out_path)
+				
+			dev_bar = tqdm(dev_loader)
+			valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
+			dev_images = []
+			for val_lr, val_hr_restore, val_hr in dev_bar:
+				batch_size = val_lr.size(0)
+				valing_results['batch_sizes'] += batch_size
+				print (val_lr.ndimension())
+				lr = Variable(val_lr)
+				hr = Variable(val_hr)
+				if torch.cuda.is_available():
+					lr = lr.cuda()
+					hr = hr.cuda()
+				
+				sr = netG(lr)
+				
+				batch_mse = ((sr - hr) ** 2).data.mean().item()
+				valing_results['mse'] += batch_mse * batch_size
+				batch_ssim = pytorch_ssim.ssim(sr, hr).item()
+				valing_results['ssims'] += batch_ssim * batch_size
+				valing_results['psnr'] = 10 * log10(1 / (valing_results['mse'] / valing_results['batch_sizes']))
+				valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
+				dev_bar.set_description(
+					desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
+						valing_results['psnr'], valing_results['ssim']))
+				
+				cache['ssim'] += valing_results['ssim']
+				cache['psnr'] += valing_results['psnr']
+				
+				# Only save 1 images to avoid out of memory 
+				if len(dev_images) < 60 :
+					dev_images.extend([to_image()(val_hr_restore.squeeze(0)), to_image()(hr.data.cpu().squeeze(0)), to_image()(sr.data.cpu().squeeze(0))])
+			
+			dev_images = torch.stack(dev_images)
+			dev_images = torch.chunk(dev_images, dev_images.size(0) // 3)
+			
+			dev_save_bar = tqdm(dev_images, desc='[saving training results]')
+			index = 1
+			for image in dev_save_bar:
+				image = utils.make_grid(image, nrow=3, padding=5)
+				utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
+				index += 1
+		
 			if use_tensorboard:			
 				log_value('ssim', cache['ssim']/len(dev_loader), epoch)
 				log_value('psnr', cache['psnr']/len(dev_loader), epoch)			
